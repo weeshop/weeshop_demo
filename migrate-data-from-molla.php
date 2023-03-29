41d407775abef004f69a14aa06ad48450658e2a0
kent@legion <164713332@qq.com>
2023-03-29 08:08:01 +0000

    Add molla product data.

--- migrate-data-from-molla.php
+++ migrate-data-from-molla.php
@@ -18,7 +18,7 @@ $product_brands_csv_target_path = __DIR__ . '/data/product_brands.csv';
 $product_categories_csv_target_path = __DIR__ . '/data/product_categories.csv';
 $product_attribute_values_csv_target_path = __DIR__ . '/data/product_attibute_values.csv';
 $products_csv_target_path = __DIR__ . '/data/products.csv';
-$product_variations_csv_target_path = __DIR__ . '/data/products.csv';
+$product_variations_csv_target_path = __DIR__ . '/data/product_variations.csv';
 
 $json = json_decode(file_get_contents($molla_json_path), TRUE);
 $product_brands_csv = 'id,name,path,parent' . PHP_EOL;
@@ -27,18 +27,26 @@ $product_attribute_values_csv = 'id,attribute,name' . PHP_EOL;
 $products_csv = 'id,stores,title,main_image,detail_images,categories,body' . PHP_EOL;
 $product_variations_csv = 'id,product,sku,title,price,attribute_clothes_color,attribute_clothes_size,stock,weight' . PHP_EOL;
 
-$product_brands = $product_categories = $product_attribute_values = [];
+$product_brands = $product_categories = [];
+$product_attribute_values_color = [];
+$product_attribute_values_size = [];
 
 foreach ($json['products'] as $product_index => $product) {
 
   // Obtain product brands.
   foreach ($product['brands'] as $product_brand) {
-    $product_brand[$product_brand['slug']] = $product_brand['name'];
+    $product_brands[$product_brand['slug']] = [
+      'name' => $product_brand['name'],
+      'slug' => $product_brand['slug'],
+    ];
   }
 
   // Obtain product categories.
   foreach ($product['category'] as $product_category) {
-    $product_categories[$product_category['slug']] = $product_category['name'];
+    $product_categories[$product_category['slug']] = [
+      'name' => $product_category['name'],
+      'slug' => $product_category['slug'],
+    ];
   }
 
   // Copy all the pictures.
@@ -67,6 +75,68 @@ foreach ($json['products'] as $product_index => $product) {
 
   $body = '"' . $product['short_desc'] . '"';
   $products_csv .= "product${product_index},1,${product['name']},${images_with_data_path[0]},${detail_images},${categories_str},${body}" . PHP_EOL;
+
+  // Obtain product attribute values.
+  foreach ($product['variants'] as $product_variation) {
+    // Obtain attribute values.
+    $product_attribute_values_color[$product_variation['color']] = [
+      'code' => $product_variation['color'],
+      'name' => $product_variation['color_name'],
+    ];
+
+    // Obtain size.
+    foreach ($product_variation['size'] as $size) {
+      $product_attribute_values_size[$size['name']] = $size['name'];
+    }
+  }
+}
+
+foreach (array_values($product_brands) as $index => $brand) {
+  $index++;
+  $product_brands_csv .= "product_brand${index},${brand['name']},/products/brand/${brand['slug']},\"\"" . PHP_EOL;
+}
+
+foreach (array_values($product_categories) as $index => $category) {
+  $index++;
+  $product_categories_csv .= "product_category${index},${category['name']},/products/category/${category['slug']},\"\"" . PHP_EOL;
+}
+
+/**
+ * Turn a string into path format.
+ *
+ * @param string $str
+ *   String to be transformed.
+ *
+ * @return string
+ *   String in path format.
+ */
+function turn_into_path_string(string $str): string {
+  return strtolower(str_replace(' ', '-', $str));
+}
+
+foreach (array_values($product_attribute_values_color) as $index => $color) {
+  $index++;
+  $product_attribute_values_csv .= "attribute_color${index},clothes_color,${color['name']}" . PHP_EOL;
+}
+
+foreach (array_values($product_attribute_values_size) as $index => $size) {
+  $index++;
+  $product_attribute_values_csv .= "attribute_size${index},clothes_size,${size}" . PHP_EOL;
+}
+
+// Make product variations for every product through all attributes.
+$variation_index = 0;
+foreach ($json['products'] as $product_index => $product) {
+  foreach (array_values($product_attribute_values_color) as $color_key => $color) {
+    $color_key++;
+    foreach (array_values($product_attribute_values_size) as $size_key => $size) {
+      $variation_index++;
+      $size_key++;
+      $color_id = turn_into_underscored($color['name']);
+      $size_id = turn_into_underscored($size);
+      $product_variations_csv .= "product_variation${variation_index},product${product_index},product${product_index}-${color_id}-${size_id},${color['name']} - ${size},100,attribute_color${color_key},attribute_size${size_key},1000,0" . PHP_EOL;
+    }
+  }
 }
 
 file_put_contents($product_brands_csv_target_path, $product_brands_csv);
@@ -80,4 +150,17 @@ file_put_contents($product_variations_csv_target_path, $product_variations_csv);
  */
 function get_suffix(string $file_name): string {
   return pathinfo($file_name, PATHINFO_EXTENSION);
+}
+
+/**
+ * Turn a string into underscored format.
+ *
+ * @param string $str
+ *   String to be transformed.
+ *
+ * @return string
+ *   String in underscored format.
+ */
+function turn_into_underscored(string $str): string {
+  return strtolower(str_replace(' ', '_', $str));
 }
